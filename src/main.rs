e4e81eed4b28d3c9c36aba440eb16165ed368d65
Giovanni Pagliani <giovannipagliani@MacBook-Pro-di-Giovanni.local>
2024-10-15 20:10:57 +0200

    ripristinato al normale

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
 fn main() {
 
     println!("Hello, world!");
-    println!("Hello, world2!");
-
-    println!("Hello, world3!");
-    println!("Hello, world4!");
-
-
-    println!("Hello, world5!");
 }
