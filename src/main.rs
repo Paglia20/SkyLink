afe4a8d7db3b9e040391a29007267b9601fe7299
Suge42 <lsandrini04@gmail.com>
2024-12-04 11:29:49 +0100

    Now the testebench works

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use crate::initializer::initialize;
+use crate::test_bench::generic_fragment_forward;
 
 mod my_drone;
 mod sim_app;
 mod sim_control;
 mod initializer;
-mod testbench;
+mod test_bench;
 
 fn main() {
-
     println!("Hello, world!");
     //sim_app::run_simulation_gui();
-    let handles = initialize("input.toml");
-    for handle in handles.into_iter() {
-        handle.join().unwrap();
+
+    let test = true;
+    if test {
+        //Comment functions we aren't testing
+        generic_fragment_forward();
+    } else {
+        let handles = initialize("input.toml");
+        for handle in handles.into_iter() {
+            handle.join().unwrap();
+        }
     }
 }
