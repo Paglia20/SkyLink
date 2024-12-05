6aedaeafd18914e717086143406052c4e2715fb7
Giovanni Pagliani <giovannipagliani@MacBook-Pro-di-Giovanni.local>
2024-12-05 18:50:46 +0100

    testings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::thread;
-use crate::test::test_bench::{my_generic_fragment_forward, test_flood};
+use crate::test::test_bench::{my_generic_fragment_forward, test_double_chain_flood, test_flood, test_star_flood};
 use crate::initializer::initialize;
 
 mod sim_app;
@@ -10,26 +10,27 @@ mod test;
 
 fn main() {
     println!("Hello, world!");
-
-    let (sim_contr, handles) = initialize("input.toml");
-    sim_app::run_simulation_gui(sim_contr);
-
-    for handle in handles.into_iter() {
-        handle.join().unwrap();
-    }
-
-    let test = false;
-    if test {
-        //Comment functions we aren't testing
-        //my_generic_fragment_forward();
-    } else {
-
-    }
+    //
+    // let (sim_contr, handles) = initialize("input.toml");
+    // sim_app::run_simulation_gui(sim_contr);
+    //
+    // for handle in handles.into_iter() {
+    //     handle.join().unwrap();
+    // }
+    //
+    // let test = false;
+    // if test {
+    //     //Comment functions we aren't testing
+    //     //my_generic_fragment_forward();
+    // } else {
+    //
+    // }
 
     // generic_chain_fragment_ack();
     // generic_fragment_drop();
     // generic_chain_fragment_drop();
 
     // test_generic();
-    test_flood();
+   // test_double_chain_flood();
+   test_star_flood();
 }
