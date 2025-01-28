e4932accacb746070eb177915796f7d7a1bd514d
Paglia20 <145615930+Paglia20@users.noreply.github.com>
2025-01-28 12:30:15 +0100

    minor

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
 
     match switch {
         Switch::SimDaniel => {
-            let (sim_contr, handles) = initialize("inputs/input_flood.toml");
+            let (sim_contr, handles) = initialize("inputs/input_star.toml");
 
             run_sim_dan(sim_contr).expect("TODO: panic message");
             for handle in handles.into_iter() {
