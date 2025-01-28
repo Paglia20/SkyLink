64cafcfe627fab433c67e4629038bcf131da4cdf
Paglia20 <145615930+Paglia20@users.noreply.github.com>
2025-01-28 12:28:27 +0100

    moved pngs

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
 
     match switch {
         Switch::SimDaniel => {
-            let (sim_contr, handles) = initialize("inputs/input_star.toml");
+            let (sim_contr, handles) = initialize("inputs/input_flood.toml");
 
             run_sim_dan(sim_contr).expect("TODO: panic message");
             for handle in handles.into_iter() {
